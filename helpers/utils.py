5a76bfe4383cf683e1e3caa8a1779e5c6d45d783
baasare <asarebernard98@gmail.com>
2023-11-14 21:55:21 -0500

    Initial commit

---
+++ helpers/utils.py
@@ -0,0 +1,496 @@
+import itertools
+import os
+import signal
+import random
+import pickle
+import codecs
+import socket
+import logging
+import requests
+import numpy as np
+import pandas as pd
+from PIL import Image
+import scipy.io as sio
+import tensorflow as tf
+from timeit import default_timer
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+from .constants import BIT_SIZE
+
+
+def post_with_retries(url, data, max_retries=3):
+    session = requests.Session()
+    retries = Retry(total=max_retries, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
+    adapter = HTTPAdapter(max_retries=retries)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    try:
+        response = session.post(url, json=data, headers={"Content-Type": "application/json"})
+        response.raise_for_status()
+        return response
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
+
+def decode_layer(b64_str):
+    return pickle.loads(codecs.decode(b64_str.encode(), "base64"))
+
+
+def encode_layer(layer):
+    return codecs.encode(pickle.dumps(layer), "base64").decode()
+
+
+def check_port(address, port):
+    # Check if the port is in use
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.bind((address, port))
+    except socket.error:
+        return True
+    finally:
+        sock.close()
+    return False
+
+
+def terminate_process_on_port(port):
+    try:
+        pid = int(os.popen(f"lsof -t -i:{port}").read())
+        os.kill(pid, signal.SIGKILL)
+        print(f"Terminated process on port {port}")
+    except Exception as e:
+        print(f"Error terminating process on port {port}: {e}")
+
+
+def fetch_index(dataset):
+    current_dir = os.path.dirname(os.getcwd())
+    path = current_dir + f'/thesis/resources/dataset/{dataset}/iid_balanced.txt'
+    clients = np.loadtxt(path, dtype=object)
+    clients = clients.astype(np.float64)
+    clients = clients.astype(np.int64)
+    return clients
+
+
+def fetch_dataset(dataset):
+    if dataset == "cifar-10":
+        return tf.keras.datasets.cifar10.load_data()
+    elif dataset == "mnist":
+        return tf.keras.datasets.mnist.load_data()
+    elif dataset == "f-mnist":
+        return tf.keras.datasets.fashion_mnist.load_data()
+    else:
+        current_dir = os.path.dirname(os.getcwd())
+        train_data = sio.loadmat(current_dir + f'/resources/dataset/svhn/train_32x32.mat')
+        test_data = sio.loadmat(current_dir + f'/resources/dataset/svhn/test_32x32.mat')
+        x_train = np.array(train_data['X'])
+        y_train = np.array(train_data['y'])
+
+        x_test = np.array(test_data['X'])
+        y_test = np.array(test_data['y'])
+
+        return (x_train, y_train), (x_test, y_test)
+
+
+def get_dataset(index, dataset, x_train, y_train, x_test, y_test):
+    if dataset == "cifar-10":
+        x_train = x_train[index]
+        y_train = y_train[index]
+
+        x_train = x_train.astype('float32') / 255.0
+        x_test = x_test.astype('float32') / 255.0
+
+        y_train = tf.keras.utils.to_categorical(y_train, 10)
+        y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+    elif dataset == 'svhn':
+        x_train = np.moveaxis(x_train, -1, 0)
+        x_test = np.moveaxis(x_test, -1, 0)
+
+        x_train = x_train[index]
+        y_train = y_train[index]
+
+        x_train = x_train.astype('float32') / 255.0
+        x_test = x_test.astype('float32') / 255.0
+
+        y_train = tf.keras.utils.to_categorical((y_train - 1), 10)
+        y_test = tf.keras.utils.to_categorical((y_test - 1), 10)
+
+    else:
+        x_train = x_train[index]
+        y_train = y_train[index]
+
+        x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32') / 255.0
+        x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32') / 255.0
+
+        y_train = tf.keras.utils.to_categorical(y_train, 10)
+        y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+    return x_train, y_train, x_test, y_test
+
+
+def get_lenet5(dataset):
+    """
+    Creates LeNet-5 model.
+
+    :param dataset: dataset being used for model
+    :return: Keras model
+    """
+
+    if dataset == "cifar-10" or dataset == 'svhn':
+        pixel = 32
+        rgb = 3
+    else:
+        pixel = 28
+        rgb = 1
+
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    seed = 1
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+
+    # LeNet-5
+    model = tf.keras.models.Sequential()
+    # Convolutional layer 1
+    model.add(tf.keras.layers.Conv2D(
+        filters=6,
+        kernel_size=(5, 5),
+        strides=1,
+        activation='relu',
+        name=f'conv2d_{0}',
+        input_shape=(pixel, pixel, rgb)
+    ))
+    model.add(tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # Convolutional layer 2
+    model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=(5, 5), activation='relu', name=f'conv2d_{1}'))
+    model.add(tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # Flatten the output of the convolutional layers
+    model.add(tf.keras.layers.Flatten())
+
+    # Fully connected layer 1
+    model.add(tf.keras.layers.Dense(units=120, activation='relu', name=f'dense_{0}'))
+
+    # Fully connected layer 2
+    model.add(tf.keras.layers.Dense(units=84, activation='relu', name=f'dense_{1}'))
+
+    # Output layer
+    model.add(tf.keras.layers.Dense(units=10, activation='softmax', name=f'dense_{2}'))
+    return model
+
+
+def get_lenet3(dataset):
+    """
+    Creates LeNet-5 model.
+
+    :param dataset: dataset being used for model
+    :return: Keras model
+    """
+
+    if dataset == "cifar-10" or dataset == 'svhn':
+        pixel = 32
+        rgb = 3
+    else:
+        pixel = 28
+        rgb = 1
+
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    seed = 1
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+
+    # LeNet-5
+    model = tf.keras.models.Sequential()
+    # Convolutional layer 1
+    model.add(tf.keras.layers.Conv2D(filters=6, kernel_size=(5, 5), strides=1, activation='relu',
+                                     input_shape=(pixel, pixel, rgb)))
+    model.add(tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # Flatten the output of the convolutional layers
+    model.add(tf.keras.layers.Flatten())
+
+    # Fully connected layer 1
+    model.add(tf.keras.layers.Dense(units=120, activation='relu'))
+
+    # Output layer
+    model.add(tf.keras.layers.Dense(units=10, activation='softmax'))
+    return model
+
+
+def generate_keys(save_path, name, nbits=BIT_SIZE):
+    """
+    :param save_path:
+    :param nbits:
+    :param name:
+    :return:
+    """
+    os.makedirs(save_path, exist_ok=True)
+
+    # Generate an RSA key pair
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=nbits
+    )
+    public_key = private_key.public_key()
+
+    public_pem_path = os.path.join(save_path, 'client_' + str(name) + '_public.pem')
+    private_pem_path = os.path.join(save_path, 'client_' + str(name) + '_private.pem')
+
+    try:
+        # Serialize the private key to PEM format and save it to a file
+        with open(private_pem_path, 'wb') as f:
+            f.write(private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+            ))
+
+        # Serialize the public key to PEM format and save it to a file
+        with open(public_pem_path, 'wb') as f:
+            f.write(public_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo
+            ))
+    except Exception as ex:
+        logging.error(ex)
+
+    return public_pem_path, private_pem_path
+
+
+def get_public_key(client_id):
+    current_dir = os.path.dirname(os.getcwd())
+    path = current_dir + f'/resources/keys/client_{str(client_id)}_public.pem'
+
+    with open(path, 'rb') as f:
+        return serialization.load_pem_public_key(f.read())
+
+
+def get_private_key(client_id):
+    current_dir = os.path.dirname(os.getcwd())
+    path = current_dir + f'/resources/keys/client_{str(client_id)}_private.pem'
+
+    with open(path, 'rb') as f:
+        return serialization.load_pem_private_key(f.read(), password=None)
+
+
+class TimingCallback(tf.keras.callbacks.Callback):
+    def __init__(self, logs=None):
+        super().__init__()
+        if logs is None:
+            logs = {}
+        self.start_time = None
+        self.logs = []
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if logs is None:
+            logs = {}
+        self.start_time = default_timer()
+
+    def on_epoch_end(self, epoch, logs=None):
+        if logs is None:
+            logs = {}
+        self.logs.append(default_timer() - self.start_time)
+
+
+def convert(imgfile, labelfile, outfile, n):
+    f = open(imgfile, "rb")
+    o = open(outfile, "w")
+    l = open(labelfile, "rb")
+
+    f.read(16)
+    l.read(8)
+    images = []
+
+    for i in range(n):
+        image = [ord(l.read(1))]
+        for j in range(28 * 28):
+            image.append(ord(f.read(1)))
+        images.append(image)
+
+    for image in images:
+        o.write(",".join(str(pix) for pix in image) + "\n")
+    f.close()
+    o.close()
+    l.close()
+
+
+def generate_additive_shares(value, n):
+    arr = np.asarray(value)
+    rand_arr = np.random.uniform(low=-np.abs(arr), high=np.abs(arr), size=(n - 1,) + arr.shape)
+    shares = np.concatenate((rand_arr, [arr - rand_arr.sum(axis=0)]), axis=0)
+    return shares
+
+
+def randomly_select_weights(weights, fraction=0.25):
+    # Get number of weights to select
+    num_select = int(weights.shape[0] * fraction)
+
+    # Generate random indices to select
+    indices = np.random.choice(weights.shape[0], size=num_select, replace=False)
+
+    return indices
+
+
+def replace_selected(weights, new_weights):
+    replaced = []
+    for w, nw in zip(weights, new_weights):
+        # Indices of weights to replace
+        indices = [...]
+        # Create copy of existing weights
+        updated = np.copy(w)
+        # Replace selected indices with new weights
+        updated[indices] = nw
+        replaced.append(updated)
+    return replaced
+
+
+def exchange_weights(client_weights):
+    n = len(client_weights)
+    client_shares = []
+
+    # Generate shares for each client's salary
+    for salary in client_weights:
+        shares = generate_additive_shares(salary, n)
+        client_shares.append(shares)
+
+    # Perform secure exchange of shares
+    exchanged_shares = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                exchanged_shares[i].append(client_shares[j][i])
+
+        exchanged_shares[i].insert(0, client_shares[i][i])
+    return exchanged_shares
+
+
+def perform_exchange(n, client_shares):
+    # Perform secure exchange of shares
+    exchanged_shares = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                exchanged_shares[i].append(client_shares[j][i])
+        exchanged_shares[i].insert(0, client_shares[i][i])
+
+    return exchanged_shares
+
+
+def combine_csv_files(experiment, dataset):
+    parent_dir = os.path.dirname((os.path.dirname(os.getcwd())))
+    folder_path = parent_dir + f'/resources/results/numerical/{experiment}/{dataset}'
+
+    # List all files in the folder
+    files = os.listdir(folder_path)
+
+    # Initialize an empty list to store the data frames
+    data_frames = []
+
+    # Iterate over each file
+    for file in files:
+        if file.startswith('client') and file.endswith('.csv'):  # Ensure that only CSV files are considered
+            file_path = os.path.join(folder_path, file)
+
+            # Read the CSV file and append it to the list
+            df = pd.read_csv(file_path)
+            df = df.drop(columns='round')
+            data_frames.append(df)
+
+    # Concatenate all data frames into one
+    if len(data_frames) != 0:
+        combined_df = pd.concat(data_frames, axis=1)
+        combined_df.to_csv(f"{folder_path}/combined.csv")
+
+    for file in files:
+        if file.startswith('client') and file.endswith('.csv'):
+            file_path = os.path.join(folder_path, file)
+            os.remove(file_path)
+
+    # return combined_df
+
+
+def combine_find_mean():
+    parent_dir = os.path.dirname((os.path.dirname(os.getcwd())))
+    folder_path = parent_dir + f'/results'
+
+    folder_items = os.listdir(folder_path)
+
+    for folder in folder_items:
+        for dataset in ['DATASET']:
+            combine_csv_files(folder, dataset)
+
+            csv_dir = os.path.join(folder_path, folder, dataset, 'combined.csv')
+
+            if os.path.exists(csv_dir):
+                df = pd.read_csv(csv_dir)
+                df['Average Accuracy'] = df.apply(lambda row: row['accuracy'].mean(), axis=1)
+                df['Average Training'] = df.apply(lambda row: row['training'].mean(), axis=1)
+                if 'secret_sharing' in df:
+                    df['Average Secret Sharing'] = df.apply(lambda row: row['secret_sharing'].mean(), axis=1)
+                df.columns = df.columns.str.replace(r'\.\d+$', '', regex=True)
+                df.to_csv(csv_dir, index=False)
+
+
+def convert_png_to_eps(folder_path):
+    files = os.listdir(folder_path)
+
+    for file in files:
+        if file.startswith('multiline') and file.endswith('.png'):  # Ensure that only png files are considered
+            # file name
+            png_file = os.path.join(folder_path, file)
+
+            # Open the PNG file
+            image = Image.open(png_file)
+
+            # Create a new EPS file with the same dimensions and mode as the PNG
+            eps_image = Image.new("RGB", image.size)
+            eps_image.paste(image)
+
+            # Save the EPS file
+            eps_image.save(png_file.replace("png", "eps"), format='EPS')
+
+
+def generate_groups(nodes, group_size):
+    # shuffle clients
+    random.shuffle(nodes)
+
+    # Create a repeating iterator over the clients list
+    client_iterator = itertools.cycle(nodes)
+
+    # Keep track of which clients have been selected
+    selected_clients = set()
+
+    # Keep selecting groups of clients until each client has been selected at least once
+    selected_groups = []
+    while len(selected_clients) < len(nodes):
+        # Select the next group of clients
+        group = list(itertools.islice(client_iterator, group_size))
+
+        # Add the selected clients to the set of selected clients
+        selected_clients.update(group)
+
+        # Add the selected group to the list of selected groups
+        selected_groups.append(group)
+
+    # All clients have been selected at least once
+    return selected_groups
+
+
+def iid_balanced(client_number, train_size, dataset):
+    # used to generate indexes
+    rand_array = np.arange(train_size)
+    np.random.shuffle(rand_array)
+
+    clients = [[] for _ in range(client_number)]
+
+    for i in range(client_number):
+        clients[i] = rand_array[
+                     int(i * train_size / client_number):int((i + 1) * train_size / client_number)]
+
+    np.savetxt(f"resources/dataset/{dataset}/iid_balanced.txt", clients)
---
+++ server_groups.py
@@ -0,0 +1,203 @@
+import time
+import uvicorn
+import pandas as pd
+import tensorflow as tf
+from fastapi import FastAPI
+from timeit import default_timer as timer
+
+from helpers.utils import check_port, terminate_process_on_port, generate_groups
+from helpers.utils import post_with_retries, encode_layer, decode_layer, get_lenet5, get_dataset
+
+from helpers.constants import MESSAGE_END_SESSION, MESSAGE_START_ASSEMBLY, MESSAGE_FL_UPDATE
+from helpers.constants import MESSAGE_SHARING_COMPLETE, ROUNDS, MESSAGE_START_TRAINING, ADDRESS
+from helpers.constants import MESSAGE_TRAINING_COMPLETED, MESSAGE_START_SECRET_SHARING, GROUPINGS
+
+
+class ServerSubGroup:
+    def __init__(self, server_id, address, port, max_nodes, client_type, dataset, indexes, x_train, y_train, x_test,
+                 y_test):
+        self.id = server_id
+        self.app = FastAPI()
+        self.port = port
+        self.address = address
+        self.connected_nodes = 0
+        self.max_nodes = max_nodes
+        self.nodes = list()
+
+        self.grouping_time = None
+        self.start_time = None
+        self.end_time = None
+        self.pending_nodes = set()
+        self.average_weights = dict()
+        _, _, self.X_test, self.y_test = get_dataset(
+            indexes[0],
+            dataset,
+            x_train,
+            y_train,
+            x_test,
+            y_test
+        )
+
+        self.global_model = get_lenet5(dataset)
+        self.max_rounds = ROUNDS
+        self.round = 0
+        self.training_completed_count = 0
+        self.client_type = client_type
+        self.dataset = dataset
+        self.groupings = list()
+        self.record = list()
+        self.current_accuracy = 0
+        self.threshold = 0
+
+        @self.app.post("/message")
+        def message(data: dict):
+            print(f"SERVER RECEIVED: {data['message']} from PORT: {data['port']}")
+
+            if data["message"] == MESSAGE_FL_UPDATE:
+                self.fl_update(data["port"], data["model_weights"])
+
+            elif data["message"] == MESSAGE_TRAINING_COMPLETED:
+                self.start_secret_sharing()
+
+            elif data["message"] == MESSAGE_SHARING_COMPLETE:
+                self.start_assembly(data["port"])
+
+            return {"status": "ok"}
+
+    def start(self):
+        if check_port(self.address, self.port):
+            terminate_process_on_port(self.port)
+
+        uvicorn.run(self.app, host="0.0.0.0", port=self.port)
+
+    def send_to_node(self, data, port=None):
+        if port is None:
+            if data["message"] == MESSAGE_START_TRAINING:
+                # if it's a start training message, then send ports to which the nodes belong to
+                for port in self.nodes:
+                    data["nodes"] = list(set(val for group in self.groupings if id in group for val in group))
+                    post_with_retries(
+                        data=data,
+                        url=f"http://{ADDRESS}:{port}/message",
+                        max_retries=3
+                    )
+                    time.sleep(2)
+            else:
+                # if any other message proceed normally
+                for port in self.nodes:
+                    post_with_retries(
+                        data=data,
+                        url=f"http://{ADDRESS}:{port}/message",
+                        max_retries=3
+                    )
+                    time.sleep(2)
+        else:
+            post_with_retries(
+                data=data,
+                url=f"http://{ADDRESS}:{port}/message",
+                max_retries=3
+            )
+
+    def start_round(self, nodes=None):
+        if nodes:
+            self.nodes = nodes
+
+        print(f'Starting round ({self.round + 1})')
+
+        temp_start_time = timer()
+        self.groupings = generate_groups(self.nodes, GROUPINGS)
+        self.grouping_time = temp_start_time - timer()
+
+        self.start_time = timer()
+        self.pending_nodes = self.nodes.copy()
+        for layer in self.global_model.layers:
+            if layer.trainable_weights:
+                self.average_weights[layer.name] = [[], []]
+
+        data = {
+            "port": "SERVER",
+            "message": MESSAGE_START_TRAINING,
+            "model_architecture": self.global_model.to_json(),
+            "model_weights": encode_layer(self.global_model.get_weights()),
+        }
+        self.send_to_node(data)
+
+    def fl_update(self, node, data):
+
+        for layer in data.keys():
+            temp_weight = decode_layer(data[layer])
+
+            if len(self.average_weights[layer][0]) == 0 and len(self.average_weights[layer][1]) == 0:
+                self.average_weights[layer][0] = temp_weight[0] / len(self.nodes)
+                self.average_weights[layer][1] = temp_weight[1] / len(self.nodes)
+            else:
+                self.average_weights[layer][0] += temp_weight[0] / len(self.nodes)
+                self.average_weights[layer][1] += temp_weight[1] / len(self.nodes)
+
+        self.pending_nodes.remove(node)
+        if not self.pending_nodes:
+            self.apply_updates()
+
+    def apply_updates(self):
+        for layer in self.global_model.layers:
+            if layer.trainable_weights:
+                layer.set_weights(self.average_weights[layer.name])
+        self.evaluate()
+
+    def evaluate(self):
+        self.global_model.compile(optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=0.001),
+                                  loss='categorical_crossentropy', metrics=['accuracy'])
+        _, self.current_accuracy = self.global_model.evaluate(self.X_test, self.y_test, verbose=0)
+        self.end_time = timer() - self.start_time
+        print('Accuracy: ', self.current_accuracy)
+        print(f'Round ({self.round + 1}) Time: {self.end_time}')
+
+        self.record.append({
+            'round': self.round + 1,
+            'accuracy': self.current_accuracy,
+            'fl': self.end_time + self.grouping_time,
+        })
+        self.end_round()
+
+    def end_round(self):
+        print("ROUND ENDED")
+        self.round += 1
+        if self.round < self.max_rounds:
+            self.start_round()
+        else:
+            self.end_session()
+
+    def end_session(self):
+        print("SESSION ENDED")
+        data = {
+            "port": "SERVER",
+            "message": MESSAGE_END_SESSION,
+            "model_weights": encode_layer(self.global_model.get_weights()),
+        }
+        pd.DataFrame(self.record).to_csv(
+            f"resources/results/{self.client_type}/{self.dataset}/server.csv",
+            index=False,
+            header=True
+        )
+
+        self.send_to_node(data)
+        terminate_process_on_port(self.port)
+
+    def start_assembly(self, port):
+        data = {
+            "port": "SERVER",
+            "message": MESSAGE_START_ASSEMBLY,
+        }
+        self.send_to_node(data, port=port)
+
+    def start_secret_sharing(self):
+        self.training_completed_count += 1
+        if self.training_completed_count == len(self.nodes):
+            self.training_completed_count = 0
+            data = {
+                "port": "SERVER",
+                "message": MESSAGE_START_SECRET_SHARING,
+            }
+            self.send_to_node(data)
+
+
