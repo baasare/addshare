f8b77bc4f402fb265e473cead47850fb72ebdc99
baasare <asarebernard98@gmail.com>
2023-11-16 19:49:36 -0500

    slurm time issues

--- slurm/job-script-fed-avg.sh
+++ slurm/job-script-fed-avg.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=20G
-#SBATCH --time=1-07:00:00
+#SBATCH --time=1-0:0
 
 # Emails me when job starts, ends or fails
 #SBATCH --mail-user=basar092@uottawa.ca
