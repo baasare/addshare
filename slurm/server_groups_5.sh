896552d9b16c5a93f0464b7d80712711bcca78d8
baasare <asarebernard98@gmail.com>
2023-11-22 12:37:12 -0500

    deployment

--- slurm/server_groups_5.sh
+++ slurm/server_groups_5.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=5
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=35G
-#SBATCH --time=0-3:00:00
+#SBATCH --time=0-12:00:00
 
 # Emails me when job starts, ends or fails
 #SBATCH --mail-user=basar092@uottawa.ca
@@ -27,7 +27,6 @@ echo ""
 module load python/3.9.6
 source ~/projects/def-pbranco/baasare/thesis/venv/bin/activate
 
-python ~/projects/def-pbranco/baasare/thesis/addshare_server_group.py cifar-10 5
 python ~/projects/def-pbranco/baasare/thesis/addshare_server_group.py f-mnist 5
 python ~/projects/def-pbranco/baasare/thesis/addshare_server_group.py mnist 5
 python ~/projects/def-pbranco/baasare/thesis/addshare_server_group.py svhn 5
