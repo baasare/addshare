99167a027747ecbf35b947612aa28f55dc1dd26a
baasare <asarebernard98@gmail.com>
2024-01-17 08:47:51 -0500

    deploy

--- slurm/addshare_encrypted.sh
+++ slurm/addshare_encrypted.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=5
 #SBATCH --cpus-per-task=30
 #SBATCH --mem=35G
-#SBATCH --time=2-00:00:00
+#SBATCH --time=5-00:00:00
 
 # Emails me when job starts, ends or fails
 #SBATCH --mail-user=basar092@uottawa.ca
